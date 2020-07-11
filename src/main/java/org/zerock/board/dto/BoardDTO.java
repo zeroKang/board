a3a5af5987b128a9aedb8d99d6289852e72adea9
cookieKang@gmail.com <cookieKang@gmail.com>
2020-07-12 01:31:44 +0900

    Test2

---
+++ src/main/java/org/zerock/board/dto/BoardDTO.java
@@ -0,0 +1,20 @@
+package org.zerock.board.dto;
+
+import java.time.LocalDateTime;
+
+public class BoardDTO {
+
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    private String writerEmail;
+
+    private String writerName;
+
+    private LocalDateTime regdate;
+
+
+}
